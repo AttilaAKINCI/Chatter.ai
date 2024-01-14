d552301bd6c14205bb915201977979568511b3b9
Attila Akinci <akinci.attila@gmail.com>
2024-01-14 18:21:21 +0100

    update ui state thread safe

--- app/src/main/kotlin/com/akinci/chatter/core/compose/UIStateExt.kt
+++ app/src/main/kotlin/com/akinci/chatter/core/compose/UIStateExt.kt
@@ -1,9 +1,10 @@
 package com.akinci.chatter.core.compose
 
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 interface UIState
 
 fun <T : UIState> MutableStateFlow<T>.reduce(reducer: T.() -> T) {
-    this.value = reducer(this.value)
+    update { reducer(it) }
 }
